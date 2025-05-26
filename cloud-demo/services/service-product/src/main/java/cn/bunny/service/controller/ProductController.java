ccc68630f09a47268c4c06004ab12466d8a7f4d6
bunny <1319900154@qq.com>
2025-05-26 18:52:27 +0800

    :sparkles: 远程调用-Feign拦截器

--- cloud-demo/services/service-product/src/main/java/cn/bunny/service/controller/ProductController.java
+++ cloud-demo/services/service-product/src/main/java/cn/bunny/service/controller/ProductController.java
@@ -3,13 +3,12 @@ package cn.bunny.service.controller;
 import cn.bunny.model.product.bean.Product;
 import cn.bunny.service.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/product")
@@ -20,12 +19,14 @@ public class ProductController {
 
     @Operation(summary = "根据id查询商品")
     @GetMapping("{id}")
-    public Product getProduct(@PathVariable("id") Long productId) {
-        try {
-            TimeUnit.SECONDS.sleep(6);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public Product getProduct(@PathVariable("id") Long productId, HttpServletRequest request) {
+        // try {
+        //     TimeUnit.SECONDS.sleep(6);
+        // } catch (InterruptedException e) {
+        //     throw new RuntimeException(e);
+        // }
+
+        System.out.println("request-X-Token: " + request.getHeader("X-Token"));
         return productService.getProductById(productId);
     }
 }
