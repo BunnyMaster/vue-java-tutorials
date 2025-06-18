39e7447e704c43f08a7b2994acbec9d4ad8c2264
bunny <1319900154@qq.com>
2025-06-19 07:25:00 +0800

    ✨ Vue2 Mixins 混入机制详解

--- vue2-tutorials/single-file/demo2/src/App.vue
+++ vue2-tutorials/single-file/demo2/src/App.vue
@@ -3,15 +3,18 @@
     <Demo1 />
     <hr />
     <Demo2 />
+    <hr />
+    <Demo3 />
   </div>
 </template>
 
 <script>
 import Demo1 from "@/views/demo1/index.vue";
 import Demo2 from "@/views/demo2/index.vue";
+import Demo3 from "@/views/demo3/index.vue";
 
 export default {
   name: "App",
-  components: { Demo1, Demo2 },
+  components: { Demo1, Demo2, Demo3 },
 };
 </script>
