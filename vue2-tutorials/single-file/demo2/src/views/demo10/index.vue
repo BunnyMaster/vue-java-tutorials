3872c8f29e192e871d9a14c378f8b0d9279775e6
bunny <1319900154@qq.com>
2025-06-21 23:53:33 +0800

    🎉 Vue2 过渡动画与 Animate.css 整合指南

--- vue2-tutorials/single-file/demo2/src/views/demo10/index.vue
+++ vue2-tutorials/single-file/demo2/src/views/demo10/index.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="card">
+  <div class="container card">
     <div class="card-header">
       <h1>动画效果:{{ visibility }}</h1>
     </div>
 
-    <Transition name="bunny" :appear="true">
-      <div class="card-body" v-show="visibility">
-        <h3>显示动画</h3>
-      </div>
-    </Transition>
+    <div class="card-body">
+      <Transition name="bunny" :appear="true">
+        <h3 v-show="visibility">显示动画</h3>
+      </Transition>
+
+      <ul>
+        <TransitionGroup
+          name="animate__animated animate__bounce"
+          appear
+          enter-active-class="animate__flipInX"
+          leave-active-class="animate__flipOutX"
+        >
+          <li key="1" v-show="visibility">嗬嗬嗬嗬嗬嗬嗬嗬嗬嗬嗬</li>
+          <li key="2" v-show="!visibility">哈哈哈哈哈哈哈哈哈哈哈</li>
+        </TransitionGroup>
+      </ul>
+    </div>
 
     <div class="card-footer">
       <div class="btn-group">
