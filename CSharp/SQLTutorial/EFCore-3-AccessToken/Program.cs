c7de1fdbe81d13b660f1ccfeb2cd7f786a62fbc4
bunny <1319900154@qq.com>
2025-06-22 23:08:52 +0800

    :sparkles: 初始化基础页面

---
+++ CSharp/SQLTutorial/EFCore-3-AccessToken/Program.cs
@@ -0,0 +1,49 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("123456789134567813")),
+        ValidIssuer = "Bunny",
+        ValidateAudience = true,
+        ValidAudience = "StudentApi",
+        ValidateLifetime = true
+    };
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+// 身份验证
+app.UseAuthentication();
+// 授权
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ CSharp/WPFTutorial/Demo-TODO/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Demo_TODO
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 最小化按钮
+            MinButton.Click += (_, _) => { WindowState = WindowState.Minimized; };
+
+            // 放大缩小按钮
+            MaxButton.Click += (_, _) =>
+            {
+                WindowState = WindowState == WindowState.Maximized ? WindowState.Minimized : WindowState.Maximized;
+            };
+
+            // 关闭按钮
+            CloseButton.Click += (_, _) => { Close(); };
+
+            // 拖拽窗口拖动
+            ColorZone.MouseDown += (_, eventArgs) =>
+            {
+                if (eventArgs.LeftButton == MouseButtonState.Pressed) DragMove();
+            };
+
+            // 双击放大和缩小
+            ColorZone.MouseDoubleClick += (_, _) =>
+            {
+                WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+            };
+        }
+    }
+}
