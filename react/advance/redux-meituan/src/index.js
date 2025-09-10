3b4049342808a77dd13a10dba0dc3c94708a244a
bunny <1319900154@qq.com>
2025-09-10 21:17:34 +0800

    :sparkles: feat(store): 实现 Redux store并优化应用架构
    - 新增 Redux store 配置和 takeaway模块
    - 重构 App组件，使用 Redux 状态管理- 更新 Menu 组件，根据 Redux 状态动态渲染
    - 添加异步获取商品列表的功能

--- react/advance/redux-meituan/src/index.js
+++ react/advance/redux-meituan/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
-import App from './App'
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
 
-
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <App />
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ react/advance/redux-meituan/src/store/modules/takeaway.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const foodsStore = createSlice({
+  name: 'foods',
+  initialState: {
+    // 商品列表
+    foodList: [],
+
+    // 菜单激活下表
+    activeIndex: 0,
+  },
+  reducers: {
+    /**
+     * 设置商品列表
+     * @param {Object} state - Redux store 的当前状态
+     * @param {Object} action - Redux action 对象
+     * @param {Array} action.payload - 商品列表数据
+     */
+    setFoodList(state, action) {
+      state.foodList = action.payload;
+    },
+
+    /**
+     * 设置菜单激活下标
+     * @param {Object} state - Redux store 的当前状态
+     * @param {Object} action - Redux action 对象
+     * @param {number} action.payload - 菜单激活下标
+     */
+    changeActiveIndex(state, action) {
+      state.activeIndex = action.payload;
+    },
+  },
+});
+
+const { setFoodList, changeActiveIndex } = foodsStore.actions;
+
+/**
+ * 异步获取外卖商品列表的 thunk 函数
+ * @param {Function} dispatch - Redux dispatch 函数，用于触发 action
+ * @returns {Promise<void>} 返回一个 Promise，不包含具体返回值
+ */
+const fetchFoodList = () => {
+  return async (dispatch) => {
+    // 发送 HTTP GET 请求获取外卖商品数据
+    const res = await axios.get('http://localhost:3004/takeaway');
+
+    // 将获取到的商品数据通过 dispatch 分发给 Redux store
+    dispatch(setFoodList(res.data));
+  };
+};
+
+const foodsReducer = foodsStore.reducer;
+
+export { changeActiveIndex, fetchFoodList };
+export default foodsReducer;
