e5d9cef2cbce632f4d590ad5f89fa40d86bb1aaf
bunny <1319900154@qq.com>
2025-09-10 23:07:38 +0800

    ✨ feat(cart): 实现购物车增删功能
    - 添加清空购物车、增加商品数量、减少商品数量的功能
    - 更新 Count 组件，增加点击事件处理
    - 修改 cartList 显示逻辑，根据购物车列表动态显示
    - 优化购物车面板显示逻辑，添加 visible 类名控制显示隐藏

--- react/advance/redux-meituan/src/components/Cart/index.js
+++ react/advance/redux-meituan/src/components/Cart/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, decreaseCartItem, increaseCartItem } from '../../store/modules/takeaway';
 import Count from '../Count';
 import './index.scss';
 
@@ -7,10 +8,11 @@ const Cart = () => {
   // 购物车列表
   const { cartList } = useSelector((state) => state.foods);
 
+  const dispatch = useDispatch();
+
   // 计算总价
   const totalPrice = cartList.reduce((a, c) => a + c.price * c.count, 0);
 
-  const cart = [];
   return (
     <div className="cartContainer">
       {/* 遮罩层 添加visible类名可以显示出来 */}
@@ -32,16 +34,21 @@ const Cart = () => {
         {/* 结算 or 起送 */}
         {cartList.length > 0 ? <div className="goToPreview">去结算</div> : <div className="minFee">¥20起送</div>}
       </div>
-      {/* 添加visible类名 div会显示出来 */}
-      <div className={classNames('cartPanel')}>
+      {/* 添加 visible 类名 div会显示出来 */}
+      <div className={classNames('cartPanel', cartList.length > 0 && 'visible')}>
         <div className="header">
           <span className="text">购物车</span>
-          <span className="clearCart">清空购物车</span>
+          <span
+            className="clearCart"
+            onClick={() => dispatch(clearCart())}
+          >
+            清空购物车
+          </span>
         </div>
 
         {/* 购物车列表 */}
         <div className="scrollArea">
-          {cart.map((item) => {
+          {cartList.map((item) => {
             return (
               <div
                 className="cartItem"
@@ -62,7 +69,11 @@ const Cart = () => {
                   </div>
                 </div>
                 <div className="skuBtnWrapper btnGroup">
-                  <Count count={item.count} />
+                  <Count
+                    count={item.count}
+                    onMinus={() => dispatch(decreaseCartItem({ id: item.id }))}
+                    onPlus={() => dispatch(increaseCartItem({ id: item.id }))}
+                  />
                 </div>
               </div>
             );
--- react/advance/redux-meituan/src/store/modules/takeaway.js
+++ react/advance/redux-meituan/src/store/modules/takeaway.js
@@ -34,21 +34,55 @@ const foodsStore = createSlice({
       state.activeIndex = action.payload;
     },
 
+    /**
+     * 向购物车中添加商品项
+     * @param {Object} state - 状态对象，包含cartList购物车列表
+     * @param {Object} action - 动作对象，payload包含要添加的商品信息
+     * @returns {void}
+     */
     addCartItem(state, action) {
       const { id } = action.payload;
+      // 查找购物车中是否已存在相同商品
       const item = state.cartList.find((item) => item.id === id);
 
       if (item) {
+        // 如果商品已存在，则数量加1
         item.count++;
       } else {
+        // 如果商品不存在，则添加新商品，初始数量为1
         action.payload.count = 1;
         state.cartList.push(action.payload);
       }
     },
+
+    increaseCartItem(state, action) {
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+
+      if (!item) return;
+
+      item.count++;
+    },
+
+    decreaseCartItem(state, action) {
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+
+      if (!item) return;
+
+      if (item.count <= 0) {
+        state.cartList = state.cartList.filter((item) => item.id !== action.payload.id);
+        return;
+      }
+
+      item.count--;
+    },
+
+    clearCart(state) {
+      state.cartList = [];
+    },
   },
 });
 
-const { setFoodList, changeActiveIndex, addCartItem } = foodsStore.actions;
+const { setFoodList, changeActiveIndex, addCartItem, increaseCartItem, decreaseCartItem, clearCart } = foodsStore.actions;
 
 /**
  * 异步获取外卖商品列表的 thunk 函数
@@ -67,5 +101,5 @@ const fetchFoodList = () => {
 
 const foodsReducer = foodsStore.reducer;
 
-export { addCartItem, changeActiveIndex, fetchFoodList };
+export { addCartItem, changeActiveIndex, clearCart, decreaseCartItem, fetchFoodList, increaseCartItem };
 export default foodsReducer;
