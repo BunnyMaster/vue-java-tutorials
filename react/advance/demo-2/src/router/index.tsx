5e4695494ea81ca8cb8ec4eed3c53c5354cb343c
John.Bunny <zhukewei@sonfir.com>
2025-09-11 11:24:01 +0800

    ✨ refactor(router): 更新路由模式并优化代码格式
    - 将 createBrowserRouter 替换为 createHashRouter，以使用 Hash 模式
    - 修正导入语句和变量声明的格式
    - 添加注释以区分不同的路由模式

--- react/advance/demo-2/src/router/index.tsx
+++ react/advance/demo-2/src/router/index.tsx
@@ -1,6 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom'; // 修正导入来源
-import routes from './routes';
+import { createHashRouter } from "react-router-dom"; // 修正导入来源
+import routes from "./routes";
 
-const router = createBrowserRouter(routes, {});
+// History 模式
+// const router = creteBrowserRouter(routes, {});
+
+// 使用 Hash 模式
+const router = createHashRouter(routes, {});
 
 export default router;
